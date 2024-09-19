9b961dc3425f4fffd6c1d63c6f266870078af294
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-09-19 02:22:35 -0400

    Update homepage
    Co-authored-by: Aarnav Khanna <aarnavkhanna@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl font-semibold text-center">Rate My Aura</h1>
