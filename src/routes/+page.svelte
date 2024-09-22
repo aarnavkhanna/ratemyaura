07755560ff80bee81fde08b41c56040dffd4b1a4
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-09-22 15:48:24 -0400

    Add displaying rating reasoning
    Co-authored-by: Aarnav Khanna <aarnavkhanna@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
     import type { PageData, ActionData } from "./$types";
     import RateForm from "./rate-form.svelte";
+
     export let data: PageData;
     export let form: ActionData;
 </script>
 
 <div class="flex flex-col justify-center items-center gap-8">
     <h1 class="text-4xl font-semibold text-center">Rate My Aura</h1>
-    <RateForm data={data.rateForm} />
-    {#if form && form.rating}
-        <div class="flex flex-col gap-4">
-            <h2 class="text-2xl font-semibold text-center">Rating</h2>
-            <p class="text-center">{form.rating}</p>
-        </div>
-    {/if}
+    <RateForm data={data.rateForm} form={form} />
 </div>
--- src/routes/rate-form.svelte
+++ src/routes/rate-form.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import type { ActionData } from "./$types";
     import * as Form from "$lib/components/ui/form";
     import { Input } from "$lib/components/ui/input";
     import { rateFormSchema, type RateFormSchema } from "./schema";
@@ -9,16 +10,20 @@
         fileProxy,
     } from "sveltekit-superforms";
     import { zodClient } from "sveltekit-superforms/adapters";
+    import * as Alert from "$lib/components/ui/alert";
+    import { Skeleton } from "$lib/components/ui/skeleton";
     import LoaderCircle from "lucide-svelte/icons/loader-circle";
+    import Sparkles from "lucide-svelte/icons/sparkles";
 
     export let data: SuperValidated<Infer<RateFormSchema>>;
+    export let form: ActionData;
 
-    const form = superForm(data, {
+    const rateForm = superForm(data, {
         validators: zodClient(rateFormSchema),
         delayMs: 100,
     });
 
-    const { form: formData, enhance, delayed } = form;
+    const { form: formData, enhance, delayed } = rateForm;
 
     const imageFiles = fileProxy(formData, "image");
     let imageURL: string = "";
@@ -36,7 +41,7 @@
         <div class="w-64 h-64 border rounded-lg"></div>
     {/if}
     <form method="POST" enctype="multipart/form-data" use:enhance class="flex flex-col justify-center items-center gap-4">
-        <Form.Field {form} name="image">
+        <Form.Field form={rateForm} name="image">
             <Form.Control let:attrs>
                 <Form.Label>Image</Form.Label>
                 <input {...attrs} type="file" bind:files={$imageFiles} />
@@ -53,4 +58,26 @@
             {/if}
         </Form.Button>
     </form>
+    {#if $delayed}
+        <Alert.Root class="max-w-96">
+            <Sparkles class="h-4 w-4" />
+            <Alert.Title>
+                <Skeleton class="h-4 w-32 mb-2" />
+            </Alert.Title>
+            <Alert.Description>
+                <Skeleton class="h-4 w-full mb-2" />
+                <Skeleton class="h-4 w-full" />
+            </Alert.Description>
+        </Alert.Root>
+    {:else if form && form.rating}
+        <Alert.Root class="max-w-96">
+            <Sparkles class="h-4 w-4" />
+            <Alert.Title>
+                Rating: {form.rating}%
+            </Alert.Title>
+            <Alert.Description>
+                AI's Reasoning: {form.ratingReasoning}
+            </Alert.Description>
+        </Alert.Root>
+    {/if}
 </div>
