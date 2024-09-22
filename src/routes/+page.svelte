7dcb16e671cada7a7f8b14435f690c286f0fe20e
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-09-22 16:34:02 -0400

    Add head tags
    Co-authored-by: Aarnav Khanna <aarnavkhanna@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,11 @@
     export let form: ActionData;
 </script>
 
+<svelte:head>
+	<title>Rate My Aura</title>
+	<meta name="description" content="Rate My Aura - An aura rater. Powered by AI." />
+</svelte:head>
+
 <div class="flex flex-col justify-center items-center gap-8">
     <h1 class="text-4xl font-semibold text-center">AI Aura Rater</h1>
     <RateForm data={data.rateForm} form={form} />
