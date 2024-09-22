24ba21d4eecee4e97bb51dd5637e8bf107d89109
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-09-22 16:30:03 -0400

    Add logo
    Co-authored-by: Aarnav Khanna <aarnavkhanna@users.noreply.github.com>

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,8 @@
-<header class="flex flex-row gap-4 p-6">
+<script lang="ts">
+    import Logo from "$lib/assets/logo.png?enhanced";
+</script>
+
+<header class="flex flex-row items-center gap-2 p-6">
+    <enhanced:img src={Logo} alt="Rate My Aura Logo" class="h-12 w-12" />
     <span class="text-2xl font-semibold">Rate My Aura</span>
 </header>
