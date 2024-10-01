8a080af49574604c3a35831473fd3ca58acb282a
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2024-09-30 22:59:17 -0400

    Add github link
    Co-authored-by: Aarnav Khanna <aarnavkhanna@users.noreply.github.com>

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
     import Logo from "$lib/assets/logo.png?enhanced";
+    import { Button } from "$lib/components/ui/button";
+    import Github from "lucide-svelte/icons/github";
 </script>
 
-<header class="flex flex-row items-center gap-2 p-6">
-    <enhanced:img src={Logo} alt="Rate My Aura Logo" class="h-12 w-12" />
-    <span class="text-2xl font-semibold">Rate My Aura</span>
+<header class="flex flex-row justify-between items-center gap-8 p-6">
+    <a class="flex flex-row items-center gap-2" href="/">
+        <enhanced:img src={Logo} alt="Rate My Aura Logo" class="w-12 h-auto" />
+        <span class="text-2xl font-semibold">Rate My Aura</span>
+    </a>
+    <Button class="p-2" variant="ghost" size="icon" href="https://github.com/aarnavkhanna/ratemyaura" target="_blank" rel="noopener noreferrer">
+        <Github class="h-6 w-6" />
+    </Button>
 </header>
